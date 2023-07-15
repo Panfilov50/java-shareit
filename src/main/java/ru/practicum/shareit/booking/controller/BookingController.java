70cfa8439631ec9517b7e11287c464ba371ac4fc
Panfilov50 <117942994+Panfilov50@users.noreply.github.com>
2023-07-15 13:27:46 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -1,8 +1,7 @@
 package ru.practicum.shareit.booking.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * TODO Sprint add-bookings.
