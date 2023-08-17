b9f53bbc2f2484f33777a187fa7bfb5888a33ebd
Panfilov50 <117942994+Panfilov50@users.noreply.github.com>
2023-08-17 19:32:10 +0300

    Update UserServiceImpl.java

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 @AllArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
